18ad405c3832382b30f44604f26c56445205b688
James White <james_white_d@hotmail.com>
2025-03-28 17:32:07 +0000

    Bump version to 3.1.0-beta.1

--- worlds/pokemon_crystal/__init__.py
+++ worlds/pokemon_crystal/__init__.py
@@ -54,7 +54,7 @@ class PokemonCrystalWorld(World):
     Explore the Johto and Kanto regions, become the Pokémon League Champion, and
     defeat the elusive Red at the peak of Mt. Silver!"""
     game = "Pokemon Crystal"
-    apworld_version = "3.0.1"
+    apworld_version = "3.1.0-beta.1"
 
     topology_present = True
     web = PokemonCrystalWebWorld()
