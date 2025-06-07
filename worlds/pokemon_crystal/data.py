b628574bb99960709ded7eea9cbd7ceb336c89d8
James White <james_white_d@hotmail.com>
2025-06-07 16:59:03 +0100

    Bump version to 4.0.0-alpha.4

--- worlds/pokemon_crystal/data.py
+++ worlds/pokemon_crystal/data.py
@@ -8,7 +8,7 @@ import yaml
 
 from BaseClasses import ItemClassification
 
-APWORLD_VERSION = "4.0.0-alpha.3"
+APWORLD_VERSION = "4.0.0-alpha.4"
 POKEDEX_OFFSET = 10000
 POKEDEX_COUNT_OFFSET = 20000
 
