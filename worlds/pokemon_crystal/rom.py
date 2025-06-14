607311543bcd5ee1efba55791ae99adab99eaef7
James White <james_white_d@hotmail.com>
2025-06-15 00:33:29 +0100

    Basepatch update to fix trainersanity and vanilla clair engine flags

--- worlds/pokemon_crystal/rom.py
+++ worlds/pokemon_crystal/rom.py
@@ -537,7 +537,7 @@ def generate_output(world: "PokemonCrystalWorld", output_directory: str, patch: 
 
     if world.options.trainersanity:
         # prevents disabling gym trainers, among a few others
-        write_bytes(patch, [1], data.rom_addresses["AP_Setting_Trainersanity"] + 1)
+        write_bytes(patch, [1], data.rom_addresses["AP_Setting_Trainersanity"] + 2)
         # removes events from certain trainers, to prevent disabling them.
         missable_trainers = ["BurglarDuncan", "BurglarEddie", "GruntM13", "GruntM11", "GruntM25", "GruntF3", "GruntM24",
                              "GruntM14", "GruntM15", "GruntM3", "GruntM4", "GruntM5", "GruntM6", "GruntF2", "GruntM7",
