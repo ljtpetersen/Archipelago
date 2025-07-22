4bea634e5b0d334a561b293d5884337d9e0e757e
James White <james_white_d@hotmail.com>
2025-07-22 02:11:52 +0100

    Add enforce wild encounters logic to slot data

--- worlds/pokemon_crystal/__init__.py
+++ worlds/pokemon_crystal/__init__.py
@@ -435,6 +435,7 @@ class PokemonCrystalWorld(World):
             "randomize_fly_unlocks",
             "fly_cheese",
             "mount_mortar_access",
+            "enforce_wild_encounter_methods_logic",
         )
         slot_data["apworld_version"] = self.apworld_version
         slot_data["tea_north"] = 1 if "North" in self.options.saffron_gatehouse_tea.value else 0
